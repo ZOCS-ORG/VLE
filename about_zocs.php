722c20e4cdae28c0b5b2853b065377da4f84578a
Kolichi <Kolichitutu110@gmail.com>
2024-01-30 09:09:09 +0200

    first comit

---
+++ about_zocs.php
@@ -0,0 +1,35 @@
+ 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>ZOCS Landing Page</title>
+
+</head>
+<body>
+<?php include ('./nav.php') ?>
+<div class="About mt-4 mb-2 text-center">
+  <h2>About ZOCS</h2>
+  <p class="">
+    Zambia Open Community Schools (ZOCS) is a vibrant and flagship organisation for Community Schools in Zambia since 1992 that empowers communities to provide quality basic education to OVC, especially girls and children with disabilities. Premised on the principle that every child has a
+    “right to education”, ZOCS, through its three-pillar approach of Advocacy, Capacity Building and Service Delivery, has accomplished much for the children and people of Zambia, reaching out to 259,691 learners (132,459 Boys and 127,232 Girls) in 1,385 demonstration Community Schools across 97 Districts in all the 10 provinces of Zambia.
+  </p>
+</div>
+
+
+</div> 
+<script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+<footer>
+        <p>&copy; 2024 ZOCS. All rights reserved.</p>
+        <p>Address: P.O Box 50429 | Lusaka</p>
+        <p>Contact: Phone: (+260) 211-253641 / 211-253656</p>
+        <p>Powered by Chesco tech-ltd</p>
+    </footer>
+</html>
+
+
