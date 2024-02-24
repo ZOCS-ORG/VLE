0cb3e432cefe4d66c651c3fa0a79fe737defba65
Kolichi <kolichitutu110@gmail.com>
2024-02-24 10:37:01 +0200

    gift  sync

--- VLE/modules/functions/teacher/parent/all_parents.php
+++ VLE/modules/functions/teacher/parent/all_parents.php
@@ -6,7 +6,12 @@ include_once('../layouts/topbar.php');
 
 $user_id = $_SESSION['id'];
 
-$school
+$school ="SELECT school_id FROM school_teachers WHERE teacher_id = $user_id";
+$school_query = mysqli_query($db,$school) or die(mysqli_error($db));
+$school_row = mysqli_fetch_array($school_query);
+
+$school_id = $school_row["school_id"];
+echo "".$school_id."";
 ?>
 
 <hr />
@@ -37,7 +42,7 @@ $school
                             LEFT JOIN school_teachers st ON u.created_by = st.teacher_id
                             LEFT JOIN schools s ON st.school_id = s.school_id
                             WHERE u.user_role LIKE '%parent%'
-                            AND s.school_id = ;";
+                            AND s.school_id = $school_id";
                             $res = mysqli_query($db, $sql) or die('An error occured: ' . mysqli_error($db));
                             while ($row = mysqli_fetch_array($res)) {
                             ?>
