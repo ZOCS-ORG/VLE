8feb1d76000c091504fef207ba1f80765bf6347e
Kolichi <kolichitutu110@gmail.com>
2024-02-26 15:56:00 +0200

    pull for moe

--- VLE/lms/includes/profile_navbar.php
+++ VLE/lms/includes/profile_navbar.php
@@ -12,7 +12,7 @@ if (empty($t_id)) {
         <li><a href="../student/stud_profile.php">Home</a></li>
         <li><a href="../student/submit.php">Submit Assignments</a></li>
         <li><a href="../student/show_notices.php">Notifications</a></li>
-        <li><a href="../student/forum.php">Forum</a></li>
+        <!-- <li><a href="../student/forum.php">Forum</a></li> -->
         <li class="divider"></a></li>
         <li><a href="../../modules/functions/<?php echo $_SESSION['role'] ?>/">Back to Dashboard </a></li>
     </ul>
@@ -27,7 +27,7 @@ if (empty($t_id)) {
 
         <ul id="nav_drop" class="dropdown-content">
             <li><a href="../<?php echo $_SESSION['role'] ?>/home.php">Home</a></li>
-            <li><a href="../<?php echo $_SESSION['role'] ?>/forum.php">Forum</a></li>
+            <!-- <li><a href="../<?php echo $_SESSION['role'] ?>/forum.php">Forum</a></li> -->
             <li class="divider"></a></li>
             <li><a href="../../modules/functions/<?php echo $_SESSION['role'] ?>/">Back to Dashboard </a></li>
         </ul>
