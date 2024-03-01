4fed69fd3b0c2fa865dfe9f9015e71e799ed2013
Kolichi <kolichitutu110@gmail.com>
2024-03-01 12:30:40 +0200

    zocs  and admin responsiveness

--- VLE/modules/functions/zocs/layouts/sidebar.php
+++ VLE/modules/functions/zocs/layouts/sidebar.php
@@ -1,5 +1,28 @@
 <!-- @Overide some fa styling -->
 <style>
+
+      /* Add custom styles for sidebar */
+      .navbar-nav.sidebar_new_bg .nav-item {
+        white-space: normal; 
+        /* overflow: clip;  */
+       
+    }
+
+    /* Adjust padding/margin as needed */
+    .navbar-nav.sidebar_new_bg .nav-item .nav-link {
+        padding: 10px; /* Adjust as needed */
+        margin: 0; /* Reset margin */
+    }
+
+    .navbar-nav.sidebar_new_bg .nav-item .nav-link span{
+       font-size: 80% !important;
+    }
+
+    /* Ensure icon and text are vertically centered */
+    .navbar-nav.sidebar_new_bg .nav-item .nav-link i {
+        vertical-align: middle;
+    }
+    
     .navbar-nav i.fas {
         color: green !important;
         font-size: 190% !important;
@@ -33,6 +56,19 @@
     .sidebar-light .nav-item .nav-link[data-toggle="collapse"]::after {
         display: none;
 
+    }
+
+
+    
+    /* Fix overlapping dropdown menu items */
+    .bg-py-2.collapse-inner.rounded {
+        position: relative;
+        z-index: 9999; /* Ensure dropdown is on top */
+    }
+
+    /* Change background color of dropdown menu */
+    .navbar-nav.sidebar_new_bg .nav-item .collapse.show {
+        background-color: lightgray;
     }
 </style>
 
